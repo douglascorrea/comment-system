c21d152a182c376377daea52445b7ff0fcb6001e
Douglas Silvio Correa <me@douglascorrea.io>
2023-06-24 13:15:22 -0300

    Tests completed

--- tests/Feature/CommentTest.php
+++ tests/Feature/CommentTest.php
@@ -11,10 +11,11 @@ class CommentTest extends TestCase
 {
 
     use RefreshDatabase;
+
     /**
      * Test create a single comment happy path
      */
-    public function test_create_single_comment(): void
+    public function test_it_should_create_single_comment(): void
     {
         $response = $this->postJson('/api/comment', ['name' => 'John Doe',
             'body' => 'Hey this is my first comment!']);
@@ -25,7 +26,7 @@ class CommentTest extends TestCase
     /**
      * Test create a comment with multiple childs and list it
      */
-    public function test_create_comment_with_childs(): void
+    public function test_it_should_create_comment_with_childs_and_list_it(): void
     {
         $parent_response = $this->postJson('/api/comment', ['name' => 'John Doe',
             'body' => 'Hey this is my first comment!']);
@@ -39,7 +40,7 @@ class CommentTest extends TestCase
 
         $second_child_response = $this->postJson('/api/comment', ['name' => 'Danny Doe',
             'body' => 'Hey this is my second reply comment!', 'parent_comment_id' => $parent_id]);
-        $first_child_response->assertStatus(201);
+        $second_child_response->assertStatus(201);
 
         $list_response = $this->getJson('/api/comment');
         $list_response->assertStatus(200);
@@ -52,6 +53,37 @@ class CommentTest extends TestCase
     }
 
     /**
+     * Test comment basic validations
+     */
+    public function test_it_should_not_create_without_name(): void
+    {
+        $response = $this->postJson('/api/comment', ['body' => 'This is a comment without a name']);
+
+        $response->assertStatus(422);
+        $response->assertJsonFragment(['name' => ['The name field is required.']]);
+
+    }
+
+    public function test_it_should_not_create_without_body(): void
+    {
+        $response = $this->postJson('/api/comment', ['name' => 'John Doe']);
+
+        $response->assertStatus(422);
+        $response->assertJsonFragment(['body' => ['The body field is required.']]);
+
+    }
+
+    public function test_it_should_not_create_invalid_parent(): void {
+        $response = $this->postJson('/api/comment', ['name' => 'John Doe',
+            'body' => 'Hey this is my first comment!',
+            'parent_comment_id' => 'invalid']);
+
+        $response->assertStatus(422);
+        $response->assertJsonFragment(['parent_comment_id' => ['The selected parent comment id is invalid.']]);
+    }
+
+
+    /**
      * Test create a single comment invalid parent
      */
     public function test_create_invalid_parent_comment(): void
@@ -61,5 +93,31 @@ class CommentTest extends TestCase
             'parent_comment_id' => 10000]);
 
         $response->assertStatus(422);
+    }
+
+    /**
+     * Test comment validation too deep (more than 3 levels)
+     */
+    public function test_it_should_not_allow_comment_too_depp(): void
+    {
+        $parent_response = $this->postJson('/api/comment', ['name' => 'John Doe',
+            'body' => 'Hey this is my first comment!']);
+        $parent_id = $parent_response['id'];
+
+        $parent_response->assertStatus(201);
+
+        $how_deep_is_allowed = config('comment.how_deep_comments_are_allowed');
+        for($i = 0; $i < $how_deep_is_allowed; $i++) {
+            $child_response = $this->postJson('/api/comment', ['name' => 'Jane Doe',
+                'body' => 'Hey this is my first reply comment!', 'parent_comment_id' => $parent_id]);
+            $child_response->assertStatus(201);
+            $parent_id = $child_response['id'];
+        }
+        $child_response = $this->postJson('/api/comment', ['name' => 'Jane Doe',
+            'body' => 'Hey this is my first reply comment!', 'parent_comment_id' => $parent_id]);
+        $child_response->assertStatus(422);
+        $child_response->assertJsonFragment(['parent_comment_id' => ['The parent comment id field is prohibited.']]);
+
+
     }
 }
